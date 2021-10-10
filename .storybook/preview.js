b3c54d3cf86a18ef2e8cb37ce2484c7404ef8703
Mahmoud <myousif123@gmail.com>
2021-10-09 20:25:41 -0400

    Added typescript support.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,4 +6,13 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
 }
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,9 +1,14 @@
 /** Because every project needs a button. */
 
-function Button({
+export interface ButtonProps {
+  /** Contents that appear in the button */
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({
   children,
   ...rest
-}) {
+}: ButtonProps) => {
   return (
     <button {...rest} >
       {children}
