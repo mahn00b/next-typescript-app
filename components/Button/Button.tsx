0fc8f1fc538aea79558fadeb65fdbb156b32906e
Mahmoud Yousif <myousif123@gmail.com>
2023-02-10 18:50:03 -0500

    updates first commit.

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,21 +1,25 @@
 /** Because every project needs a button. */
+import type { HTMLAttributes } from 'react';
+import cx from 'classnames';
+import styles from './Button.module.scss';
 
-export interface ButtonProps {
+export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   /** Contents that appear in the button */
   children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({
+const Button = ({
   children,
+  className,
   ...rest
 }: ButtonProps) => {
   return (
-    <button {...rest} >
+    <button className={cx(styles.Button, className)} {...rest} >
       {children}
     </button>
   )
 }
 
-Button.displayName = 'Button';
+// Button.displayName = 'Button';
 
 export default Button;
