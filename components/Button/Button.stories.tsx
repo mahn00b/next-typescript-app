1421d8c5476779f4421b57ee7cbbf9558dc99943
Mahmoud Yousif <myousif123@gmail.com>
2023-03-06 14:23:21 -0500

    Storybook upgrades and other chores:
    - Upgraded storybook using cli
    - Added new addon packages for sass processing in storybook
    - Upgrading storybook addon packages to be compatible with React 18
    - Added style-loader and css-loader to suppport sass in storybook
    - Migrated storybook to use webpack v5 instead v4
    - Refactored storybook config files to use typescript
    - Added the fsevents@latest to mitigate issues with outdated transient dependencies. (Read more: https://github.com/fsevents/fsevents/issues/181)
    - Updated the example Button component story config to leverage storybook's typescript defs.

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,10 +1,11 @@
-import Button, { ButtonProps } from './Button';
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import Button from './Button';
 
 export default {
   component: Button,
   title: 'Button'
-}
+} as ComponentMeta<typeof Button>
 
-const Template = (args: ButtonProps) => <Button {...args} >Press Me</Button>
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} >Press Me</Button>
 
 export const Primary = Template.bind({})
