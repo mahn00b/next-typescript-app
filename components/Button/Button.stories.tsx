8b7d8cbbe5a2d667cc7b89deab4fde73ea9c5c76
Mahmoud Yousif <mahn00b@users.noreply.github.com>
2023-04-16 12:41:49 -0400

    Fixed linting issues. (#8)
    * Fixed linting issues.
    - Cleared up outdated prettier usage.
    - Fixed eslint config.
    - Added jest eslint
    
    * Removed nodemon file and usage.

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,11 +1,12 @@
-import type { ComponentStory, ComponentMeta } from '@storybook/react';
 import Button from './Button';
+
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
 
 export default {
   component: Button,
-  title: 'Button'
-} as ComponentMeta<typeof Button>
+  title: 'Button',
+} as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} >Press Me</Button>
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args}>Press Me</Button>;
 
-export const Primary = Template.bind({})
+export const Primary = Template.bind({});
--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -1,15 +1,14 @@
-import { render } from '@testing-library/react';
 import Button from './Button';
+import { render } from '@testing-library/react';
 
 describe('Button', () => {
+  it('should be defined', () => {
+    expect(Button).toBeDefined();
+  });
 
-    it('should be defined', () => {
-        expect(Button).toBeDefined();
-    });
+  it('should render a button.', () => {
+    const { getByText } = render(<Button>TEST</Button>);
 
-    it('should render a button.', () => {
-        const { getByText } = render(<Button>TEST</Button>);
-
-        expect(getByText('TEST')).toBeInTheDocument();
-    })
-})
+    expect(getByText('TEST')).toBeInTheDocument();
+  });
+});
